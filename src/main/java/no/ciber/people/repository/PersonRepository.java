90ddd23064f29ff05825f0145c520a8aeefe684b
Michael Johansen <michaeljohansen83@gmail.com>
2013-10-12 19:06:58 +0200

    Added search method in repository

--- src/main/java/no/ciber/people/repository/PersonRepository.java
+++ src/main/java/no/ciber/people/repository/PersonRepository.java
@@ -1,7 +1,10 @@
 package no.ciber.people.repository;
 
 import no.ciber.people.model.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.math.BigInteger;
@@ -12,5 +15,6 @@ import java.math.BigInteger;
  * Time: 14:58
  */
 @RestResource(path = "person")
-public interface PersonRepository extends JpaRepository<Person,BigInteger>{
+public interface PersonRepository extends JpaRepository<Person, BigInteger> {
+    Page<Person> findByFirstNameContains(@Param("firstName") String firstName, Pageable pageable);
 }
