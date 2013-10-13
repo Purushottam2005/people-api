b5fe8362c5f52329f26ea4e976a7ec01c67ba94c
Michael Johansen <michaeljohansen83@gmail.com>
2013-10-13 13:55:39 +0200

    Added query methods

--- src/main/java/no/ciber/people/repository/PersonRepository.java
+++ src/main/java/no/ciber/people/repository/PersonRepository.java
@@ -4,6 +4,7 @@ import no.ciber.people.model.Person;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -14,7 +15,15 @@ import java.math.BigInteger;
  * Date: 12.10.13
  * Time: 14:58
  */
-@RestResource(path = "person")
+@RestResource(path = "people",rel = "people")
 public interface PersonRepository extends JpaRepository<Person, BigInteger> {
-    Page<Person> findByFirstNameContains(@Param("firstName") String firstName, Pageable pageable);
+    Page<Person> findByFirstNameIgnoreCaseContains(@Param("firstName") String firstName, Pageable pageable);
+
+    Page<Person> findByLastNameIgnoreCaseContains(@Param("lastName") String firstName, Pageable pageable);
+
+    @Query("FROM Person p WHERE UPPER(CONCAT(p.firstName, ' ', p.lastName )) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Person> nameContains(@Param("name") String name, Pageable pageable);
+
+    @Query("FROM Person p WHERE LENGTH(CONCAT(p.firstName, ' ', p.lastName )) < :nameLength")
+    Page<Person> nameShorterThan(@Param("nameLength") Integer nameLength, Pageable pageable);
 }
