a1c75ac799f27a462382b781e0d570bfca11bad3
Michael Johansen <michaeljohansen83@gmail.com>
2013-10-17 21:52:49 +0200

    Switching to simpler webappinitializer

--- api/src/main/java/no/ciber/people/config/RestExporterWebInitializer.java
+++ api/src/main/java/no/ciber/people/config/RestExporterWebInitializer.java
@@ -1,26 +1,21 @@
 package no.ciber.people.config;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
-public class RestExporterWebInitializer implements WebApplicationInitializer {
+public class RestExporterWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext rootCtx = new AnnotationConfigWebApplicationContext();
-        rootCtx.register(RestConfiguration.class);
-        servletContext.addListener(new ContextLoaderListener(rootCtx));
-
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(rootCtx);
-        ServletRegistration.Dynamic reg = servletContext.addServlet("rest-exporter", dispatcherServlet);
-        reg.setLoadOnStartup(1);
-        reg.addMapping("/*");
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
     }
 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{PersistenceConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{RestConfiguration.class};
+    }
 }
